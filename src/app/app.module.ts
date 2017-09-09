88f83da7ccbf02b4635bfeec0a9a853f01537969
jhonarias <jhfrarval@gmail.com>
2017-09-09 17:02:24 -0500

    Cambios generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { RouterModule, Routes } from '@angular/router';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
+
+import { IndexComponent } from './customers/index/index.component';
+import { AddComponent as addOrdersComponent} from './orders/add/add.component';
+
+const appRoutes: Routes = [
+  { path: 'customers/index', component: IndexComponent },
+  { path: 'orders/add', component: addOrdersComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
